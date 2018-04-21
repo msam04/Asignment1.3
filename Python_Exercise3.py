890c3b5688e16e83a2556f880f5d0e55c74686b5
msam04 <msam04@users.noreply.github.com>
2018-04-21 03:05:09 -0400

    Initial commit

---
+++ Python_Exercise3.py
@@ -0,0 +1,11 @@
+
+# coding: utf-8
+
+# In[10]:
+
+
+first_name = input("Please enter first name: ")
+last_name = input("Please enter last name: ")
+reversed_name = "".join(reversed(first_name)) + " " + "".join(reversed(last_name))
+print(reversed_name)
+
